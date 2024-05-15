6cf9e884f4bcc6ff0fc54ea35e9fd543081c9972
Aleksey Sidorov <sauron1987@gmail.com>
2024-05-16 02:47:02 +0300

    Update fdb cmake builder

--- flake.nix
+++ flake.nix
@@ -14,16 +14,80 @@
     , treefmt-nix
     }: flake-utils.lib.eachDefaultSystem (system:
     let
+      localOverlay = (import ./.);
+
       # Setup nixpkgs
       pkgs = import nixpkgs {
         inherit system;
         overlays = [
           nixpkgs-cross-overlay.outputs.overlays.default
-          (import ./.)
+          localOverlay
         ];
       };
+
       # Eval the treefmt modules from ./treefmt.nix
       treefmt = (treefmt-nix.lib.evalModule pkgs ./treefmt.nix).config.build;
+
+      # FoundationDB configuration for local launch.
+      fdb = rec {
+        port = "4689";
+        cluster = pkgs.writeText "fdb.cluster" "test1:testdb1@127.0.0.1:${port}";
+      };
+
+      mkDockerImage = crossSystem:
+        let
+          # Setup pkgs for cross compilation
+          pkgsCross = pkgs.mkCrossPkgs {
+            src = nixpkgs;
+            inherit crossSystem;
+            localSystem = system;
+            overlays = [
+              localOverlay
+            ];
+          };
+          dockerTools = pkgsCross.pkgsBuildHost.dockerTools;
+
+          entryPoint = pkgsCross.writeShellScriptBin "entry-point.sh"
+            ''
+              mkdir -p /var/foundationdb/logs
+              mkdir -p /var/foundationdb/data
+
+              fdbcli -C ${fdb.cluster} --exec "configure new single memory; status details" &
+              fdbserver -p 0.0.0.0:${fdb.port} \
+                --datadir /var/foundationdb/data -C ${fdb.cluster} \
+                --logdir /var/foundationdb/logs
+            '';
+        in
+        dockerTools.buildLayeredImage {
+          name = "foundationdb";
+          tag = "latest";
+
+          contents = with pkgsCross; [
+            fdbPackages.foundationdb73
+            # Certificates
+            dockerTools.usrBinEnv
+            dockerTools.binSh
+            dockerTools.caCertificates
+            dockerTools.fakeNss
+            # Utilites like ldd and bash to help image debugging
+            stdenv.cc.libc_bin
+            coreutils
+            bashInteractive
+            nano
+            entryPoint
+          ];
+
+          config = {
+            Cmd = [ "/bin/entry-point.sh" ];
+            WorkingDir = "/";
+            Expose = fdb.port;
+          };
+        };
+
+      loadDockerImage = { dockerImage }: pkgs.writeScriptBin "load-docker" ''
+        docker=${pkgs.docker}/bin/docker
+        docker load -i "${dockerImage}"
+      '';
     in
     {
       # for `nix fmt`
@@ -31,10 +95,24 @@
       # for `nix flake check`
       checks.formatting = treefmt.check self;
       # Overlay with the foundationdb packages.
-      overlays.default = import ./.;
-      
+      overlays.default = localOverlay;
+
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          fdbPackages.foundationdb73
+        ];
+
+        env = {
+          FDB_CLUSTER_FILE = "${fdb.cluster}";
+        };
+      };
+
       packages = {
         foundationdb73 = pkgs.fdbPackages.foundationdb73;
+        dockerImage_aarch64 = loadDockerImage {
+          dockerImage = mkDockerImage { config = "aarch64-unknown-linux-gnu"; };
+        };
+        dockerImage_x86_64 = mkDockerImage { config = "x86_64-unknown-linux-gnu"; };
       };
     });
 }
--- pkgs/foundationdb/cmake.nix
+++ pkgs/foundationdb/cmake.nix
@@ -1,6 +1,12 @@
 # This builder is for FoundationDB CMake build system.
 
-{ lib
+{ version
+, rev ? "refs/tags/${version}"
+, hash
+, officialRelease ? false
+, patches ? [ ]
+
+, lib
 , fetchFromGitHub
 , cmake
 , ninja
@@ -12,17 +18,13 @@
 , darwin
 , writeShellScriptBin
 , jemalloc
+, openssl
+, boost178
 
-, gccStdenv
-, llvmPackages
 , stdenv
-, useClang ? true
-, ...
 }:
 
 let
-  stdenv = if useClang then llvmPackages.libcxxStdenv else gccStdenv;
-
   dylib_suffix = stdenv.hostPlatform.extensions.sharedLibrary;
   isCross = stdenv.hostPlatform != stdenv.buildPlatform;
 
@@ -34,93 +36,86 @@ let
     in
     isOdd patch;
 
-  makeFdb =
-    { version
-    , hash
-    , rev ? "refs/tags/${version}"
-    , officialRelease ? false
-    , patches ? [ ]
-    , boost
-    , ssl
-    }: stdenv.mkDerivation {
-      pname = "foundationdb";
-      inherit version;
-
-      src = fetchFromGitHub {
-        owner = "apple";
-        repo = "foundationdb";
-        inherit rev hash;
-      };
-
-      buildInputs = [ ssl boost msgpack-cxx toml11 jemalloc ]
-        ++ lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.Foundation ]
-      ;
-
-      nativeBuildInputs = [ pkg-config cmake ninja python3 mono ]
-        ++ lib.optionals isCross [
-        # The simplest and almost incorrect way to provide the proper 'strip' executable.
-        (writeShellScriptBin
-          "strip"
-          ''${stdenv.cc.targetPrefix}strip "$@"'')
-      ];
-
-      cmakeFlags = [
-        (lib.optionalString officialRelease "-DFDB_RELEASE=TRUE")
-
-        # Disable CMake warnings for project developers.
-        "-Wno-dev"
-
-        # CMake Error at fdbserver/CMakeLists.txt:332 (find_library):
-        # >   Could not find lz4_STATIC_LIBRARIES using the following names: liblz4.a
-        "-DSSD_ROCKSDB_EXPERIMENTAL=FALSE"
-
-        # FoundationDB's CMake is hardcoded to pull in jemalloc as an external
-        # project at build time.
-        "-DUSE_JEMALLOC=FALSE"
-        # FIXME: why can't openssl be found automatically?
-        "-DOPENSSL_USE_STATIC_LIBS=FALSE"
-        "-DOPENSSL_CRYPTO_LIBRARY=${ssl.out}/lib/libcrypto${dylib_suffix}"
-        "-DOPENSSL_SSL_LIBRARY=${ssl.out}/lib/libssl${dylib_suffix}"
-      ];
-
-      hardeningDisable = [ "fortify" ];
-
-      env.NIX_CFLAGS_COMPILE = toString [
-        # Needed with GCC 12
-        "-Wno-missing-template-keyword"
-        # Needed to compile on aarch64
-        (lib.optionalString stdenv.isAarch64 "-march=armv8-a+crc")
-      ];
-
-      inherit patches;
-
-      # the install phase for cmake is pretty wonky right now since it's not designed to
-      # coherently install packages as most linux distros expect -- it's designed to build
-      # packaged artifacts that are shipped in RPMs, etc. we need to add some extra code to
-      # cmake upstream to fix this, and if we do, i think most of this can go away.
-      postInstall = ''
-        mv $out/sbin/fdbmonitor $out/bin/fdbmonitor
-        mkdir $out/libexec && mv $out/usr/lib/foundationdb/backup_agent/backup_agent $out/libexec/backup_agent
-        mv $out/sbin/fdbserver $out/bin/fdbserver
-
-        rm -rf $out/etc $out/lib/foundationdb $out/lib/systemd $out/log $out/sbin $out/usr $out/var
-
-        # move results into multi outputs
-        mkdir -p $dev $lib
-        mv $out/include $dev/include
-        mv $out/lib $lib/lib
-      '';
-
-      outputs = [ "out" "dev" "lib" ];
-
-      meta = with lib; {
-        description = "Open source, distributed, transactional key-value store";
-        homepage = "https://www.foundationdb.org";
-        license = licenses.asl20;
-        platforms = [ "x86_64-linux" ]
-          ++ lib.optionals (!(avxEnabled version)) [ "aarch64-linux" "aarch64-darwin" ];
-        maintainers = with maintainers; [ thoughtpolice lostnet ];
-      };
-    };
 in
-makeFdb
+stdenv.mkDerivation {
+  pname = "foundationdb";
+  inherit version;
+
+  src = fetchFromGitHub {
+    owner = "apple";
+    repo = "foundationdb";
+    inherit rev hash;
+  };
+
+  buildInputs = [
+    openssl
+    boost178
+    msgpack-cxx
+    toml11
+    jemalloc
+  ]
+  ++ lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.Foundation ]
+  ;
+
+  nativeBuildInputs = [ pkg-config cmake ninja python3 mono ]
+    ++ lib.optionals isCross [
+    # The simplest and almost incorrect way to provide the proper 'strip' executable.
+    (writeShellScriptBin
+      "strip"
+      ''${stdenv.cc.targetPrefix}strip "$@"'')
+  ];
+
+  cmakeFlags = [
+    (lib.optionalString officialRelease "-DFDB_RELEASE=TRUE")
+    # Disable CMake warnings for project developers.
+    "-Wno-dev"
+    # Disable some options that cause compilation errors
+    "-DUSE_JEMALLOC=FALSE"
+    "-DBUILD_DOCUMENTATION=FALSE"
+    "-DSSD_ROCKSDB_EXPERIMENTAL=FALSE"
+    "-DOPEN_FOR_IDE=FALSE"
+    # FIXME: why can't openssl be found automatically?
+    "-DOPENSSL_USE_STATIC_LIBS=FALSE"
+    "-DOPENSSL_CRYPTO_LIBRARY=${openssl.out}/lib/libcrypto${dylib_suffix}"
+    "-DOPENSSL_SSL_LIBRARY=${openssl.out}/lib/libssl${dylib_suffix}"
+  ];
+
+  hardeningDisable = [ "fortify" ];
+
+  env.NIX_CFLAGS_COMPILE = toString [
+    # Needed with GCC 12
+    "-Wno-missing-template-keyword"
+    # Needed to compile on aarch64
+    (lib.optionalString stdenv.isAarch64 "-march=armv8-a+crc")
+  ];
+
+  inherit patches;
+
+  # the install phase for cmake is pretty wonky right now since it's not designed to
+  # coherently install packages as most linux distros expect -- it's designed to build
+  # packaged artifacts that are shipped in RPMs, etc. we need to add some extra code to
+  # cmake upstream to fix this, and if we do, i think most of this can go away.
+  postInstall = ''
+    mv $out/sbin/fdbmonitor $out/bin/fdbmonitor
+    mkdir $out/libexec && mv $out/usr/lib/foundationdb/backup_agent/backup_agent $out/libexec/backup_agent
+    mv $out/sbin/fdbserver $out/bin/fdbserver
+
+    rm -rf $out/etc $out/lib/foundationdb $out/lib/systemd $out/log $out/sbin $out/usr $out/var
+
+    # move results into multi outputs
+    mkdir -p $dev $lib
+    mv $out/include $dev/include
+    mv $out/lib $lib/lib
+  '';
+
+  outputs = [ "out" "dev" "lib" ];
+
+  meta = with lib; {
+    description = "Open source, distributed, transactional key-value store";
+    homepage = "https://www.foundationdb.org";
+    license = licenses.asl20;
+    platforms = [ "x86_64-linux" ]
+      ++ lib.optionals (!(avxEnabled version)) [ "aarch64-linux" "aarch64-darwin" ];
+    maintainers = with maintainers; [ thoughtpolice lostnet ];
+  };
+}
